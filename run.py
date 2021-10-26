e829ae6f3be78a2aadce13c87aedb2d431cf5b2c
Logan Cantin <logan.cantin@gmail.com>
2021-10-26 17:30:15 -0400

    Created all propositions
    This commit created all propositions and populated them into data structures that allow them to be more easily turned into constraints.
    The only constraint that was added was the constraint that each tile must be filled with exactly one colour.

--- run.py
+++ run.py
@@ -1,4 +1,11 @@
 
+# Native imports
+import pprint
+
+# Project imports
+from utils import Point, LineSegment
+
+# Third party imports
 from bauhaus import Encoding, proposition, constraint
 from bauhaus.utils import count_solutions, likelihood
 
@@ -6,41 +13,105 @@ from bauhaus.utils import count_solutions, likelihood
 E = Encoding()
 
 # To create propositions, create classes for them first, annotated with "@proposition" and the Encoding
-@proposition(E)
-class BasicPropositions:
 
-    def __init__(self, data):
-        self.data = data
+# TOP RIGHT IS ORIGIN, x increases to the right, y increases down
+SIZE = 5
+COLS = ['red', 'green', 'blue', 'yellow', 'pink']
+
+@proposition(E)
+class FilledPropn:
+
+    def __init__(self, loc: Point, col):
+        self.loc = loc
+        self.col = col
+    
+    def __repr__(self):
+        return f'FILLED: position {self.loc}, colour {self.col}'
+
+
+# Dict: Point -> List[Filled]
+fill_by_point = dict()
+for x in range(SIZE):
+    for y in range(SIZE):
+        loc = Point(x, y)
+        fill_by_point[loc] = [FilledPropn(loc, col) for col in COLS]
+
+
+@proposition(E)
+class EndpointPropn:
+
+    def __init__(self, loc, col):
+        self.loc = loc
+        self.col = col
 
     def __repr__(self):
-        return f"A.{self.data}"
+        return f'ENDPOINT: position {self.loc}, color {self.col}'
+
+# Dict: Point -> List[EndpointPropn]
+endpoints_by_location = dict()
+
+# Dict: Col -> List[EndpointPropn]
+endpoints_by_col = {col: list() for col in COLS}
+
+for x in range(SIZE):
+    for y in range(SIZE):
+        loc = Point(x, y)
+        propns = list()
+        for col in COLS:
+            propn = EndpointPropn(loc, col)
+            endpoints_by_col[col].append(propn)
+            propns.append(propn)
+        endpoints_by_location[loc] = propns
 
 
-# Different classes for propositions are useful because this allows for more dynamic constraint creation
-# for propositions within that class. For example, you can enforce that "at least one" of the propositions
-# that are instances of this class must be true by using a @constraint decorator.
-# other options include: at most one, exactly one, at most k, and implies all.
-# For a complete module reference, see https://bauhaus.readthedocs.io/en/latest/bauhaus.html
-@constraint.at_least_one(E)
 @proposition(E)
-class FancyPropositions:
+class LineSegmentPropn:
 
-    def __init__(self, data):
-        self.data = data
+    def __init__(self, line_segment: LineSegment, col):
+        
+        if line_segment.manhattan_distance() != 1:
+            raise ValueError(f'Line segment is not valid! The endpoints must be adjacent (manhattan distance of 1)')
+        if not line_segment.in_bounds(SIZE, SIZE):
+            raise ValueError(f'Line segment {line_segment} is not in bounds.')
+        
+        self.line_segment = line_segment
+        self.col = col
 
     def __repr__(self):
-        return f"A.{self.data}"
+        return f'LINE SEGMENT: line segment {self.line_segment}, col {self.col}'
 
-# Call your variables whatever you want
-a = BasicPropositions("a")
-b = BasicPropositions("b")   
-c = BasicPropositions("c")
-d = BasicPropositions("d")
-e = BasicPropositions("e")
-# At least one of these will be true
-x = FancyPropositions("x")
-y = FancyPropositions("y")
-z = FancyPropositions("z")
+# Dict: LineSegment -> List[LineSegmentPropn]
+line_segment_propns_by_line_segment = dict()
+
+# Dict: Point -> List[LineSegmentPropn]
+line_segment_propns_by_point = dict()
+
+def update_line_segment_propns_by_point(loc, propns):
+    if loc not in line_segment_propns_by_point.keys():
+        line_segment_propns_by_point[loc] = propns.copy()
+    else:
+        line_segment_propns_by_point[loc].append(propns)
+
+for x in range(SIZE):
+    for y in range(SIZE):
+        loc1 = Point(x, y)
+
+        for loc2 in [Point(x + 1, y), Point(x, y + 1)]:
+            ls = LineSegment(loc1, loc2)
+            if not ls.in_bounds(SIZE, SIZE):
+                continue
+            propns = [LineSegmentPropn(ls, col) for col in COLS]
+            line_segment_propns_by_line_segment[ls] = propns
+            update_line_segment_propns_by_point(loc1, propns)
+            update_line_segment_propns_by_point(loc2, propns)
+
+
+# CONSTRAINT
+
+# Every tile must have exactly one color
+for pt in fill_by_point.keys():
+    constraint.add_exactly_one(E, *(fill_by_point[pt]))
+    
 
 
 # Build an example full theory for your setting and return it.
@@ -64,6 +135,7 @@ def example_theory():
 
 if __name__ == "__main__":
 
+    '''
     T = example_theory()
     # Don't compile until you're finished adding all your constraints!
     T = T.compile()
@@ -79,3 +151,11 @@ if __name__ == "__main__":
         # Literals are compiled to NNF here
         print(" %s: %.2f" % (vn, likelihood(T, v)))
     print()
+    '''
+
+    T = E.compile()
+    print("\nSatisfiable: %s" % T.satisfiable())
+    #print("# Solutions: %d" % count_solutions(T))
+    #print("   Solution: %s" % T.solve())
+    pprint.pprint(T.solve())
+
